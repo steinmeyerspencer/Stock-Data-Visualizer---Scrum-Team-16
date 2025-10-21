efffd5a711044bbca940e7cd9f558a974ec1e679
steinmeyerspencer <spencersteinmeyer@gmail.com>
2025-10-21 13:41:03 -0500

    Outlining project

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 ## this can be our main file
 
+# function to fetch data through API connection
+
+# function to get and validate user input
+
+# function to parse data and send data to graph
+
+# function to display graph to users browser
+
+# call user input function, use that input to fetch data, send data to graph, display graph
